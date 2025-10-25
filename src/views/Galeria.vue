f48ef420aea73f4bf509faa6aba85ddb76d5ae3d
Gustavo Santos <gustavo.santos23703@gmail.com>
2025-10-25 17:15:47 -0300

    Colocando imagens galeria

--- src/views/Galeria.vue
+++ src/views/Galeria.vue
@@ -9,16 +9,20 @@
         <img src="../assets/photos/galeria/1.png" />
         <img src="../assets/photos/galeria/2.png" />
         <img src="../assets/photos/galeria/3.png" />
-      </div>
-      <div id="photo-column">
         <img src="../assets/photos/galeria/4.png" />
         <img src="../assets/photos/galeria/5.png" />
-        <img src="../assets/photos/galeria/6.png" />
       </div>
       <div id="photo-column">
+        <img src="../assets/photos/galeria/6.gif" />
         <img src="../assets/photos/galeria/7.png" />
         <img src="../assets/photos/galeria/8.png" />
         <img src="../assets/photos/galeria/9.png" />
+      </div>
+      <div id="photo-column">
+        <img src="../assets/photos/galeria/10.png" />
+        <img src="../assets/photos/galeria/12.png" />
+        <img src="../assets/photos/galeria/13.png" />
+        <img src="../assets/photos/galeria/14.png" />
       </div>
     </div>
   </section>
